25603ad1df3c69d933b5354ecd7976bd14ddf8cf
akilic9 <68067749+akilic9@users.noreply.github.com>
2021-02-17 18:01:00 +0300

    Add files via upload

---
+++ Assets/scripts/Experiment.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Experiment : MonoBehaviour
+{
+    
+    public void EndRestart()
+    {
+        SceneManager.LoadScene("Level01");
+    }
+}
